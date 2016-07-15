e37d7dd23b68edd3faa88ff8a1335f281303970a
Ryan Uber <ru@ryanuber.com>
2016-07-14 18:56:13 -0700

    Split things out

---
+++ bucket.go
@@ -0,0 +1,68 @@
+package iocap
+
+import "time"
+
+// bucket is a simple "leaky bucket" abstraction to provide a way to
+// limit the number of operations (in this case, byte reads/writes)
+// allowed within a given interval.
+type bucket struct {
+	tokenCh chan struct{}
+	opts    RateOpts
+	drained time.Time
+}
+
+// newBucket creates a new bucket to use for readers and writers.
+func newBucket(opts RateOpts) *bucket {
+	return &bucket{
+		tokenCh: make(chan struct{}, opts.N),
+		opts:    opts,
+	}
+}
+
+// wait is used to wait for n tokens to fit into the bucket. The token
+// insert is best-effort, and the actual number of tokens inserted is
+// returned. This allows grabbing a bulk of tokens in a single pass.
+// wait will block until at least one token is inserted.
+func (b *bucket) wait(n int) (v int) {
+	// Call a non-blocking drain up-front to make room for tokens.
+	b.drain(false)
+
+	for v < n {
+		select {
+		case b.tokenCh <- struct{}{}:
+			v++
+		default:
+			if v == 0 {
+				// Call a blocking drain, because the bucket cannot
+				// make progress until the drain interval arrives.
+				b.drain(true)
+				continue
+			}
+			return
+		}
+	}
+	return
+}
+
+// drain is used to drain the bucket of tokens. If wait is true, drain
+// will wait until the next drain cycle and then continue. Otherwise,
+// drain only drains the bucket if it is due.
+func (b *bucket) drain(wait bool) {
+	if wait {
+		delay := b.drained.Add(b.opts.D).Sub(time.Now())
+		time.Sleep(delay)
+	}
+
+	if time.Since(b.drained) >= b.opts.D {
+		defer func() {
+			b.drained = time.Now()
+		}()
+		for i := 0; i < b.opts.N; i++ {
+			select {
+			case <-b.tokenCh:
+			default:
+				return
+			}
+		}
+	}
+}
--- iocap.go
+++ iocap.go
@@ -5,12 +5,6 @@ import (
 	"time"
 )
 
-// RateOpts is used to encapsulate rate limiting options.
-type RateOpts struct {
-	D time.Duration
-	N int
-}
-
 // Reader implements the io.Reader interface and limits the rate at which
 // bytes come off of the underlying source reader.
 type Reader struct {
@@ -72,72 +66,13 @@ func (w *Writer) Write(p []byte) (n int, err error) {
 	return
 }
 
+// RateOpts is used to encapsulate rate limiting options.
+type RateOpts struct {
+	D time.Duration
+	N int
+}
+
 // PerSecond returns a RateOpts configured to allow n bytes per second.
 func PerSecond(n int) RateOpts {
 	return RateOpts{time.Second, n}
-}
-
-// bucket is a simple "leaky bucket" abstraction to provide a way to
-// limit the number of operations (in this case, byte reads/writes)
-// allowed within a given interval.
-type bucket struct {
-	tokenCh chan struct{}
-	opts    RateOpts
-	drained time.Time
-}
-
-// newBucket creates a new bucket to use for readers and writers.
-func newBucket(opts RateOpts) *bucket {
-	return &bucket{
-		tokenCh: make(chan struct{}, opts.N),
-		opts:    opts,
-	}
-}
-
-// wait is used to wait for n tokens to fit into the bucket. The token
-// insert is best-effort, and the actual number of tokens inserted is
-// returned. This allows grabbing a bulk of tokens in a single pass.
-// wait will block until at least one token is inserted.
-func (b *bucket) wait(n int) (v int) {
-	// Call a non-blocking drain up-front to make room for tokens.
-	b.drain(false)
-
-	for v < n {
-		select {
-		case b.tokenCh <- struct{}{}:
-			v++
-		default:
-			if v == 0 {
-				// Call a blocking drain, because the bucket cannot
-				// make progress until the drain interval arrives.
-				b.drain(true)
-				continue
-			}
-			return
-		}
-	}
-	return
-}
-
-// drain is used to drain the bucket of tokens. If wait is true, drain
-// will wait until the next drain cycle and then continue. Otherwise,
-// drain only drains the bucket if it is due.
-func (b *bucket) drain(wait bool) {
-	if wait {
-		delay := b.drained.Add(b.opts.D).Sub(time.Now())
-		time.Sleep(delay)
-	}
-
-	if time.Since(b.drained) >= b.opts.D {
-		defer func() {
-			b.drained = time.Now()
-		}()
-		for i := 0; i < b.opts.N; i++ {
-			select {
-			case <-b.tokenCh:
-			default:
-				return
-			}
-		}
-	}
 }
