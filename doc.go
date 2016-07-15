0dd14f65718157972da7cfd03160e3def142bc5a
Ryan Uber <ru@ryanuber.com>
2016-07-15 12:52:16 -0700

    Fix doc.go

--- doc.go
+++ doc.go
@@ -1,8 +1,6 @@
-package iocap
-
 /*
 Package iocap provides rate limiting for data streams using the familiar
-`io.Reader` and `io.Writer` interfaces.
+io.Reader and io.Writer interfaces.
 
 Rate limits are expressed in bytes per second by calling PerSecond:
 
@@ -11,12 +9,13 @@ Rate limits are expressed in bytes per second by calling PerSecond:
 Readers and Writers are created by passing in an existing io.Reader or
 io.Writer along with a rate:
 
-	reader := iocap.NewReader(r, rate)
-	writer := iocap.NewWriter(w, rate)
+	r = iocap.NewReader(r, rate)
+	w = iocap.NewWriter(w, rate)
 
 Rate limiting can also be applied to an http.Handler or http.ResponseWriter
 using the wrapper methods:
 
-	h := iocap.LimitHTTPHandler(http.FileServer(http.Dir(".")), rate)
-	w := iocap.LimitResponseWriter(w, rate)
+	h = iocap.LimitHandler(h, rate)
+	w = iocap.LimitResponseWriter(w, rate)
 */
+package iocap
